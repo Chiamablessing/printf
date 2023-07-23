83523ffe1a08937b40df4b97ff7b93df8db873a7
detse233 <ebuatsie@gmail.com>
2023-07-23 16:01:24 -0700

    Adding handle_print and utils file to repo

---
+++ utils.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+ * is_printable - Evaluates if a char is printable
+ * @c: charater value
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: buffer
+ * @i: index value
+ * @ascii_code: code
+ * Return: Always 1
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: character
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+/**
+ * convert_size_number - convert number to size
+ * @num: number to convert
+ * @size: size
+ *
+ * Return: cnverted number
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+/**
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: number value
+ * @size: size value
+ *
+ * Return: Converted value
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
