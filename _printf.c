3392e5c88506319b637f6056d82adadf01caabdd
Chiamablessing <chiamablessing@gmail.com>
2023-07-26 13:57:09 +0100

    Corections and code clean-up

--- _printf.c
+++ _printf.c
@@ -26,7 +26,10 @@ int _printf(const char *format, ...)
 				format++;
 		}
 		if (*format == '\0')
+		{
+			printed_values++;
 			return (printed_values);
+		}
 		putchar(*format);
 		printed_values++;
 		format++;
